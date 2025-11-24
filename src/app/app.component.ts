4c12d39fb7e3bc254fe88949af488697b221efc5
eva.arceale <eva.arceale@plexus.es>
2025-11-24 13:36:59 +0100

    add HeroesListComponent with service integration and routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesListComponent } from "./components/heroes-list/heroes-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'heroes/list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'heroes/list',
+    component: HeroesListComponent
+  }
+];
